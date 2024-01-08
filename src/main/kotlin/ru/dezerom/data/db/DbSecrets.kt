e6efb8cac86a1080ae3da3da41e1fe5cb38d3ca5
DeZerom <demid2001s@gmail.com>
2024-01-08 14:39:21 +0300

    Убрал игнорируемый файл

--- src/main/kotlin/ru/dezerom/data/db/DbSecrets.kt
+++
@@ -1,6 +0,0 @@
-package ru.dezerom.data.db
-
-object DbSecrets {
-    const val DB_URL = "postgres://pttwbltu:RMd5rJ7zeexHTe28nWw6WIWaxqYOJcxY@balarama.db.elephantsql.com/pttwbltu"
-    const val FOO = ""
-}
